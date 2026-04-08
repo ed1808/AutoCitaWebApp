f3e09137f65f8114bfb3492a2db96ca753de225e
Edward Rodriguez <ed.rodriguez9408@gmail.com>
2026-04-07 21:15:41 -0500

    Agregar constructor adicional a UsuarioNoEncontradoException para manejar nombre de usuario

--- Backend/Core/Exceptions/UsuarioNoEncontradoException.cs
+++ Backend/Core/Exceptions/UsuarioNoEncontradoException.cs
@@ -3,4 +3,5 @@ namespace AutoCita.Api.Core.Exceptions;
 public class UsuarioNoEncontradoException : Exception
 {
     public UsuarioNoEncontradoException(Guid usuarioId) : base($"El usuario {usuarioId} no ha sido encontrado") {}
+    public UsuarioNoEncontradoException(string nombreUsuario) : base($"El usuario {nombreUsuario} no ha sido encontrado") {}
 }
